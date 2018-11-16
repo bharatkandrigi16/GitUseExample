dc8308eae51318c7a0a6ff3c5704dde4d63f6c8e
Kandrigi, Bharat <218119@s020rmc148-14.local>
2018-11-16 12:04:04 -0600

    New UI Label for - new label branch

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,10 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBAction func buttonClicked(_sender: UIButton)
-    {
-        print("This is a Git tutorial")
-    }
+
 
 }
 
