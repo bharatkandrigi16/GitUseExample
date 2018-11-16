9073ccca2380627b90ea6279e5d33654a4452295
Kandrigi, Bharat <218119@s020rmc148-14.local>
2018-11-16 11:32:20 -0600

    Committed with Map Feature File and new branch.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Kandrigi, Bharat on 11/16/18.
+//  Copyright © 2018 Kandrigi, Bharat. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
